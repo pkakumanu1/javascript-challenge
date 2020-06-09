2d3a57b7d7b16a11067c6d18ef700cfd6d4dda77
Owner <owner@Owners-MacBook-Pro.local>
2020-06-09 15:34:41 -0400

    Java Script Changes

---
+++ UFO Level1/static/js/app.js
@@ -0,0 +1,63 @@
+// from data.js
+var tableData = data;
+
+
+
+// declare variable - tell you to start at tbody
+var body = d3.select("#ufo-table>tbody");
+var text = d3.select("#datetime");
+var button = d3.select("#filter-btn");
+var form = d3.select(".form-group");
+var state = d3.select("#state");
+
+
+tableData.forEach(function (record) {
+    //console.log(record);
+    var row = body.append('tr');
+    Object.entries(record).forEach(function ([key, value]) {
+        //console.log(key,value);
+        var col = row.append('td').text(value);
+    });
+});
+
+
+
+// connect an event handler on click and on enter
+
+text.on("change", runEnter);
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter(event) {
+    var inputValue = document.getElementById("datetime").value
+    var stateValue = document.getElementById("state").value
+    console.log(inputValue);
+    console.log(stateValue);
+    // Prevent the page from refreshing
+    
+    d3.event.preventDefault();
+    //define a variable to get the input
+
+    
+    function filterD(ufo) {
+        return ufo.datetime === inputValue;
+    }
+
+    var filteredData = tableData.filter(filterD);
+    //var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+
+    body.html("");
+
+    filteredData.forEach(function (record) {
+        console.log(record);
+        var row = body.append('tr');
+        Object.entries(record).forEach(function ([key, value]) {
+            console.log(key, value);
+            var col = row.append('td').text(value);
+        });
+    });
+    d3.event.preventDefault();
+};
+// connects to a function that will a) clear the data b) filter data c) display the data -> table
